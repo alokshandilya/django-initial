eff50083c530a7370e80dfdbf2924ca9bc636758
alokshandilya <alok.shandilya10@gmail.com>
2024-10-11 04:13:03 +0530

    feat: add hello and sum views, update urls.py
    - path("sum/<int:num1>/<int:num2>/", sum)
      - can be replaced by <str:num1> and <str:num2> to accept any string,
        therefore allowing for negative numbers and decimals and later
        converting them to integers in the view via int(num1) and int(num2)

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import about, index
+from .views import about, hello, index, sum
 
 urlpatterns = [
     path("", index),
     path("about/", about),
+    path("hello/<str:user_name>/", hello),
+    path("sum/<int:num1>/<int:num2>/", sum),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -8,3 +8,11 @@ def index(request):
 
 def about(request):
     return HttpResponse("About page")
+
+
+def hello(request, user_name):
+    return HttpResponse(f"Hello, {user_name}")
+
+
+def sum(request, num1, num2):
+    return HttpResponse(f"sum: {num1 + num2}")
